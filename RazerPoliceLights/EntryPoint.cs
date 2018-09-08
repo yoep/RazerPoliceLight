dd72e4296823aa850136d61faeac89ec93e9f7a9
Yoeri <yoeri.peeters10@telenet.be>
2018-09-08 13:54:29 +0200

    Initial commit

---
+++ RazerPoliceLights/EntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using Rage;
+
+[assembly: Rage.Attributes.Plugin("Razer Police Lights Keyboard", Description = "Razer Keyboard lighting effect")]
+
+namespace RazerPoliceLights
+{
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            GameFiber.StartNew(VehicleListener.Start);
+        }
+    }
+}
---
+++ RazerPoliceLights/VehicleListener.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Rage;
+
+namespace RazerPoliceLights
+{
+    public class VehicleListener
+    {
+        private Ped _player;
+
+        private VehicleListener()
+        {
+            this._player = Game.LocalPlayer.Character;
+            KeepAlive();
+        }
+
+        public static void Start()
+        {
+            try
+            {
+                new VehicleListener();
+            }
+            catch (Exception exception)
+            {
+                Console.Out.WriteLine(exception);
+            }
+        }
+
+        private void KeepAlive()
+        {
+            while (true)
+            {
+                if (IsPlayerDriving())
+                {
+                    
+                }
+
+                GameFiber.Yield();
+            }
+        }
+
+        private bool IsPlayerDriving()
+        {
+            Vehicle playerLastVehicle = _player.LastVehicle;
+
+            return playerLastVehicle.Driver == _player;
+        }
+    }
+}
